e13d5c5c3fdfbc2408121bc2aa5d57e7d3a8877f
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-10-20 13:20:58 +0900

    Feature customer factory function.

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -1,12 +1,36 @@
 package aggregate
 
 import (
+	"errors"
+
+	"github.com/google/uuid"
 	"github.com/hirasawayuki/go-ddd/entity"
 	"github.com/hirasawayuki/go-ddd/value_object"
+)
+
+var (
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
 )
 
 type Customer struct {
 	person       *entity.Person
 	products     []*entity.Item
 	transactions []value_object.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]value_object.Transaction, 0),
+	}
 }
