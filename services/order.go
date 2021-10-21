1035e087463df30b40facab101c08197ef135178
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-10-21 21:37:45 +0900

    feature OrderService

---
+++ services/order.go
@@ -0,0 +1,20 @@
+package services
+
+import "github.com/hirasawayuki/go-ddd/domain/customer"
+
+type OrderConfiguration func(os *OrderService) error
+
+type OrderService struct {
+	customers customer.CustomerRepository
+}
+
+func NewOrderService(cfgs ...OrderConfiguration) (*OrderService, error) {
+	os := &OrderService{}
+	for _, cfg := range cfgs {
+		err := cfg(os)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return os, nil
+}
