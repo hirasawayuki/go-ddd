700ecfde328697494d38320517f2802182a115a3
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-11-07 17:57:01 +0900

    Add unit test for order service

---
+++ services/order_test.go
@@ -0,0 +1,59 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/hirasawayuki/go-ddd/aggregate"
+)
+
+func init_products(t *testing.T) []aggregate.Product {
+	beer, err := aggregate.NewProduct("Beer", "Healthy Beverage", 1.99)
+	if err != nil {
+		t.Error(err)
+	}
+	peenuts, err := aggregate.NewProduct("Peenuts", "Healthy Snacks", 0.99)
+	if err != nil {
+		t.Error(err)
+	}
+	wine, err := aggregate.NewProduct("Wine", "Healthy Wine", 0.99)
+	if err != nil {
+		t.Error(err)
+	}
+	products := []aggregate.Product{
+		beer, peenuts, wine,
+	}
+	return products
+}
+
+func TestOrder_NewOrderService(t *testing.T) {
+	products := init_products(t)
+
+	os, err := NewOrderService(
+		WithMemoryCustomerRepository(),
+		WithMemoryProductRepository(products),
+	)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	cust, err := aggregate.NewCustomer("Percy")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.customers.Add(cust)
+	if err != nil {
+		t.Error(err)
+	}
+
+	order := []uuid.UUID{
+		products[0].GetID(),
+	}
+
+	_, err = os.CreateOrder(cust.GetID(), order)
+	if err != nil {
+		t.Error(err)
+	}
+}
